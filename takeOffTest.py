d82ea479e4b18c70558af5ee2c3371fdde2695ed
Santiago Behak <sbehak@gmail.com>
2018-08-12 16:19:56 -0300

    take off test

---
+++ takeOffTest.py
@@ -0,0 +1,26 @@
+"""
+Demos the tricks on the bebop. Make sure you have enough room to perform them!
+
+Author: Amy McGovern
+"""
+
+from pyparrot.Bebop import Bebop
+
+bebop = Bebop()
+
+print("connecting")
+success = bebop.connect(10)
+print(success)
+
+print("sleeping")
+bebop.smart_sleep(5)
+
+bebop.ask_for_state_update()
+
+bebop.safe_takeoff(10)
+
+bebop.smart_sleep(5)
+bebop.safe_land(10)
+
+print("DONE - disconnecting")
+bebop.disconnect()
