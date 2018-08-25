834b3d26dd44a62fe2f967bf4cbde9e413232ba4
grondan <grondan93@gmail.com>
2018-08-25 20:36:07 -0300

    actualizarMapa y cancelarMision

--- stateMachine/states/cancelarMision.py
+++ stateMachine/states/cancelarMision.py
@@ -1,11 +1,17 @@
+from stateMachine.statesEnum import ASIGNAR_POI, CANCELAR_MISION
+from connections import client
+from utils import createMessage
+from connections.message_type import MISSION_ABORTED
+
 class cancelarMision():
-    def __init__(self):
+    def __init__(self, bebop, dataBuffer, previousState):
+        self.bebop = bebop
+        self.client = dataBuffer
 
-    def getNextState():
+    def getNextState(self):
+        return ASIGNAR_POI
 
-        if cancelarMisionTransitions.isBateriaBaja():
-            actualState = BATERIA_BAJA
-        elif cancelarMisionTransitions.isBateriaCritica():
-            actualState = BATERIA_CRITICA
-        elif cancelarMisionTransitions.isExplorar():
-            actualState = EXPLORAR
+
+    def execute(self):
+        self.client.send_message(createMessage(CANCELAR_MISION, MISSION_ABORTED, self.bebop.ip))
+        return None
