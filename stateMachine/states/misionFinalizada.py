19ff1d756d87b61fe0155380cd3d3027e255f8ec
Martin <martin816@gmail.com>
2018-08-25 15:14:37 -0300

    Estado mision finalizada

--- stateMachine/states/misionFinalizada.py
+++ stateMachine/states/misionFinalizada.py
@@ -1,6 +1,14 @@
+from stateMachine.statesEnum import ATERRIZAR
+from pyparrot.Bebop import Bebop
+
 class misionFinalizada():
-    def __init__(self):
+    def __init__(self, bebop, dataBuffer, previousState):
         self.nextState = ATERRIZAR
+        self.bebop = bebop
 
     def getNextState():
         return self.nextState
+        
+    def execute(self):
+        self.bebop.move(self.bebop.home)
+        return None
