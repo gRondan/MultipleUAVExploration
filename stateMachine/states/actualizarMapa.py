17c2ad8adb919a2610e5daaac31ed98b6d28aa57
grondan <grondan93@gmail.com>
2018-08-26 20:26:54 -0300

    timers para pois

--- stateMachine/states/actualizarMapa.py
+++ stateMachine/states/actualizarMapa.py
@@ -1,21 +1,28 @@
 from stateMachine.statesEnum import ENVIAR_MENSAJES
+from threading import Timer
+from properties import POI_TIMERS, POI_POSITIONS
 
 
 class actualizarMapa():
-    def __init__(self, bebop, dataBuffer, previousState, timerDrones, messages):
+    def __init__(self, bebop, dataBuffer, previousState, timerPOIs, timeoutPoi, messages):
         self.nextState = ENVIAR_MENSAJES
         self.bebop = bebop
-        self.timerDrones = timerDrones
         self.previousState = previousState
+        self.timerPOIs = timerPOIs
         self.messages = messages
         self.dataBuffer = dataBuffer
+        self.timeoutPoi = timeoutPoi
 
     def getNextState(self):
         return self.nextState
 
     def execute(self):
-        if (self.bebop.poi_position == self.bebop.home):
-            del self.timerDrones[self.bebop.poi_position]
+        current_position = self.bebop.current_position
+        if (self.bebop.poi_position == current_position):
+            timeout = POI_TIMERS[POI_POSITIONS.index(current_position)]
+            executionTimer = Timer(timeout, self.isEndMision)
+            executionTimer.start()
+            self.timerPOIs.remove(current_position)
         self.bebop.updateSearchMap(self.bebop.current_position)
         return self.dataBuffer
 
