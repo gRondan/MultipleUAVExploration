dbd6757485b891f8f954995c3e3b3937f6dd2c37
Santiago Behak <sbehak@gmail.com>
2018-08-26 15:10:12 -0300

    drone class linted

--- flightplans/drone.py
+++ flightplans/drone.py
@@ -1,19 +1,19 @@
 import properties
 import utils
 import math
-from random import randint
 from batteryEnum import LOW, CRITICAL, NORMAL
 import threading
 from pyparrot.Bebop import Bebop
 import random
 import time
 
+
 class drone:
-    def __init__(self, ip):
+    def __init__(self, home, ip):
         self.rango_largo = properties.RANGO_LARGO
         self.rango_ancho = properties.RANGO_ANCHO
-        self.mapa_largo = properties.MAPA_LARGO/self.rango_largo
-        self.mapa_ancho = properties.MAPA_ANCHO/self.rango_ancho
+        self.mapa_largo = properties.MAPA_LARGO / self.rango_largo
+        self.mapa_ancho = properties.MAPA_ANCHO / self.rango_ancho
         self.ip = ip
         self.search_map = [[0 for j in range(int(self.mapa_largo))]for i in range(int(self.mapa_ancho))]
         self.current_position = home
@@ -36,7 +36,7 @@ class drone:
 
     def move(self, new_position):
         dx, dy = new_position[0] - self.current_position[0], new_position[1] - self.current_position[1]
-        real_dx, real_dy = dx*self.rango_ancho, dy*self.rango_largo
+        real_dx, real_dy = dx * self.rango_ancho, dy * self.rango_largo
         self.bebop.move_relative(real_dx, real_dy, 0, 0)
         time.sleep(2)
         self.current_position = new_position
@@ -49,25 +49,22 @@ class drone:
         self.poi_position = poiPosition
 
     def explore(self, forcePosition):
-        #self.updateSearchMap(self.current_position)
-        exitloop = False
+        # self.updateSearchMap(self.current_position)
         firstTime = True
         x = self.current_position[0]
         y = self.current_position[1]
         best_values = []
-        for y2 in range(-1,2):
-            for x2 in range(-1,2):
-                x3 = x+x2
-                y3 = y+y2
+        for y2 in range(-1, 2):
+            for x2 in range(-1, 2):
+                x3 = x + x2
+                y3 = y + y2
                 if self.validatePosition(x3, y3, forcePosition):
                     if firstTime:
                         self.mutex_search_map.acquire()
                         val = self.search_map[x3][y3]
                         self.mutex_search_map.release()
-                        x1 = x3
-                        y1 = y3
                         firstTime = False
-                    print("x3: "+str(x3)+ " y3: "+str(y3)+" self.mapa_ancho: "+str(self.mapa_ancho)+ " self.mapa_largo: "+ str(self.mapa_largo))
+                    print("x3: " + str(x3) + " y3: " + str(y3) + " self.mapa_ancho: " + str(self.mapa_ancho) + " self.mapa_largo: " + str(self.mapa_largo))
                     self.mutex_search_map.acquire()
                     if (self.search_map[x3][y3] == val):
                         best_values.append((x3, y3))
@@ -78,42 +75,41 @@ class drone:
                         val = self.search_map[x3][y3]
                     self.mutex_search_map.release()
         selected = self.selectBestValue(best_values)
-        print("x: "+str(selected[0])+" y: "+str(selected[1]))
+        print("x: " + str(selected[0]) + " y: " + str(selected[1]))
         return selected
-
 
     def selectBestValue(self, best_values):
         lenght = len(best_values)
         if(lenght == 1):
             return best_values[0]
         else:
-            selected = random.randint(0, lenght-1)
+            selected = random.randint(0, lenght - 1)
             return best_values[selected]
 
     def validatePosition(self, x3, y3, forcePosition):
-        condition = x3>=0 and y3>= 0 and x3<self.mapa_ancho and y3<self.mapa_largo
-        tupla = (x3,y3)
-        if (forcePosition != None):
+        condition = x3 >= 0 and y3 >= 0 and x3 < self.mapa_ancho and y3 < self.mapa_largo
+        tupla = (x3, y3)
+        if (forcePosition is not None):
             return (condition and self.minDistanceToTarget(self.home, self.current_position, tupla))
-        elif self.poi_position != None:
+        elif self.poi_position is not None:
             return (condition and self.minDistanceToTarget(self.poi_position, self.current_position, tupla))
         elif self.checkBatteryStatus() == NORMAL:
             return condition
         else:
             return (condition and self.minDistanceToTarget(self.home, self.current_position, tupla))
 
-    def minDistanceToTarget(target, positionA, positionB):
+    def minDistanceToTarget(self, target, positionA, positionB):
         distance2 = self.calculateDistance(target, positionA)
         distance1 = self.calculateDistance(target, positionB)
-        print("distance1: "+str(distance1)+ " distance2: "+ str(distance2))
-        return (distance1 < distance2 )
+        print("distance1: " + str(distance1) + " distance2: " + str(distance2))
+        return (distance1 < distance2)
 
     def calculateDistance(self, tuple1, tuple2):
         return math.sqrt((tuple2[1] - tuple1[1])**2 + (tuple2[0] - tuple1[0])**2)
 
     def updateSearchMap(self, tupla):
         self.mutex_search_map.acquire()
-        self.search_map[tupla[0]][tupla[1]] +=1
+        self.search_map[tupla[0]][tupla[1]] += 1
         self.mutex_search_map.release()
 
     def getBatteryPercentage(self):
@@ -131,14 +127,13 @@ class drone:
     def goHome(self):
         self.move(self.home)
 
-
     def getNewCoordinate(self, pos):
         x1 = self.current_position[pos]
         if self.current_position[pos] < self.home[pos]:
             x1 = self.current_position[pos] + 1
         elif self.current_position[pos] > self.home[pos]:
-            x1 = self.current_position[pos]-1
+            x1 = self.current_position[pos] - 1
         return x1
 
-    def disconnect():
+    def disconnect(self):
         self.bebop.disconnect()
