67c61c49d0c7735886743e3a66160f19214ca0fa
Martin <martin816@gmail.com>
2018-09-23 12:19:22 -0300

    Ruta test altura

---
+++ testAltitudeSensor.py
@@ -0,0 +1,29 @@
+from flightplans import drone
+from properties import HOME
+import time
+
+# def main():
+#     drone1 = drone.drone(HOME)
+#     drone1.bebop.connect(10)
+#     drone1.bebop.ask_for_state_update()
+
+#     def sensorCallback(drone):
+#         print("bebop.sensors.attitude: " + str(drone.bebop.sensors.sensors_dict["AltitudeChanged_altitude"]))
+
+#     drone1.bebop.set_user_sensor_callback(sensorCallback, drone1)
+#     drone1.bebop.safe_takeoff(10)
+#     time.sleep(10)
+#     drone1.bebop.safe_land(10)
+def sensorCallback(drone):
+	print("bebop.sensors.altitude: " + str(drone.bebop.sensors.sensors_dict["AltitudeChanged_altitude"]))
+
+def main():
+    drone1 = drone.drone(HOME)
+    drone1.bebop.connect(10)
+    drone1.bebop.ask_for_state_update()
+    drone1.bebop.set_user_sensor_callback(sensorCallback, drone1)
+    drone1.bebop.safe_takeoff(10)
+    time.sleep(10)
+    drone1.bebop.safe_land(10)
+
+main()
