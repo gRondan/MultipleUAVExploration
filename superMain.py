f9e82ac6823043c92e82697c3fdca9ecaffad2d9
Santiago Behak <sbehak@gmail.com>
2018-10-27 12:59:45 -0300

    coordinacion de superMains

--- superMain.py
+++ superMain.py
@@ -4,12 +4,12 @@ import time
 from enums import SH_TIMESTAMP, RANDOM, SH_NO_GREEDY_TIMESTAMP
 
 algoritmos = [SH_TIMESTAMP, RANDOM, SH_NO_GREEDY_TIMESTAMP]
-ejecuciones = 2
+ejecuciones = 1
 # las instancias y los algoritmos arrancan en el 0 y van agarran el intervalo [min, max)
-instancia_min = 1
-instancia_max = 3
-algoritmo_min = 1
-algoritmo_max = 2
+instancia_min = 11
+instancia_max = 21
+algoritmo_min = 0
+algoritmo_max = 3
 ejecucion_inicial = 0
 
 print("ALGORITMOS: ", algoritmos)
@@ -23,8 +23,19 @@ for index, algoritmo in enumerate(algoritmos):
         print('PROBANDO ALGORITMO: ', algoritmo)
         for instancia in range(instancia_min, instancia_max):
             for ejecucion in range(ejecucion_inicial, ejecuciones):
+                with open('superMainLock.txt', 'a') as f:
+                    f.write('LOCK')
                 cwd = os.path.join(os.getcwd(), "main.py")
                 cmd = cwd + ' ' + str(instancia) + " " + str(ejecucion)
                 print('ALGORITMO: {}, MAPA: {}, ITERACION: {}'.format(algoritmo, instancia, ejecucion))
                 os.system('{} {}'.format('python', cmd))
+                content = ''
+                with open('superMainLock.txt', 'r') as f:
+                    content = f.read()
+                content = content.replace('LOCK', '', 1)
+                with open('superMainLock', 'w') as f:
+                    f.write(content)
+                while content != '':
+                    with open('superMainLock.txt', 'r') as f:
+                        content = f.read()
             ejecucion_inicial = 0
